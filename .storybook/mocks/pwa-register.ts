b5a6af5e38283019ed9777a1768421e21952f350
nitesh6c <abnermyerssara@outlook.com>
2026-07-12 18:49:09 -0700

    Deploy BC Champs competition site

--- .storybook/mocks/pwa-register.ts
+++
@@ -1,8 +0,0 @@
-type RegisterSWOptions = {
-  onNeedRefresh?: () => void;
-  onOfflineReady?: () => void;
-};
-
-export function registerSW(_options?: RegisterSWOptions) {
-  return async (_reloadPage?: boolean) => {};
-}
